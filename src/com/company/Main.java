e96253fc4a1201fc6f842fb1a09e587ca36da0b3
ulyssesfed <34658540+ulyssesfed@users.noreply.github.com>
2022-10-03 14:02:54 +0000

    taxes!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,62 +7,63 @@ public class Main {
 
     static int loginfails = 0;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-       if( login(getString("username"), getString("Password: "))){
-        System.out.println("hello " + type);
-        menu(); //if the login is successful, the menu is called
+        if (login(getString("username"), getString("Password: "))) {
+            System.out.println("hello " + type);
+            menu(); // if the login is successful, the menu is called
 
+        } else {
+            System.out.println("Login Failed");
+            loginfails++; // increments the number of login fails
+            if (loginfails == 3) {// if the number of login fails is 3, the program exits
+                System.out.println("Too many failed attempts");
+                System.exit(0);// exits the program
+            } else {
+                main(args);// if the number of login fails is less than 3, the program restarts
+            }
 
-       }
-       else {
-           System.out.println("Login Failed");
-           loginfails++; //increments the number of login fails
-              if (loginfails == 3){//if the number of login fails is 3, the program exits
-                System.out.println("Too many failed attempts"); 
-                System.exit(0);//exits the program
-              }
-              else {
-                main(args);//if the number of login fails is less than 3, the program restarts
-              }
-           
-       }
-	
-    }
-    static int getInt(String prompt){ //gets an integer from the user
-        Scanner scanner = new Scanner(System.in);
-            System.out.println(prompt);
-            return scanner.nextInt();
-        
-    }
-    static String getString(String prompt){ //gets a string from the user
-        Scanner scanner = new Scanner(System.in);
-            System.out.println(prompt);
-            return scanner.nextLine();
         }
 
-        public static double getDouble(String string) { //gets a double from the user
-            Scanner scanner = new Scanner(System.in);
-            System.out.println(string);
-            return scanner.nextDouble();
-        }
+    }
 
-    private static boolean login(String username, String password){
-        String[][] users = { //the array of users
-                {"admin", "admin"},
-                {"user", "user"},
-                {"guest", "guest"}
+    static int getInt(String prompt) { // gets an integer from the user
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(prompt);
+        return scanner.nextInt();
+
+    }
+
+    static String getString(String prompt) { // gets a string from the user
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(prompt);
+        return scanner.nextLine();
+    }
+
+    public static double getDouble(String string) { // gets a double from the user
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(string);
+        return scanner.nextDouble();
+    }
+
+    private static boolean login(String username, String password) {
+        String[][] users = { // the array of users
+                { "admin", "admin" },
+                { "user", "user" },
+                { "guest", "guest" }
         };
-        for (int i = 0; i < users.length; i++){ //loops through the array of users
-            if (users[i][0].equals(username) && users[i][1].equals(password)){ //checks if the username and password match
-                type = users[i][0]; //sets the type of user
-                return true; //returns true if the username and password match
+        for (int i = 0; i < users.length; i++) { // loops through the array of users
+            if (users[i][0].equals(username) && users[i][1].equals(password)) { // checks if the username and password
+                                                                                // match
+                type = users[i][0]; // sets the type of user
+                return true; // returns true if the username and password match
             }
         }
-        return false; //if the username and password don't match, it returns false
-        
+        return false; // if the username and password don't match, it returns false
+
     }
-    static void menu(){ //the menu
+
+    static void menu() { // the menu
         System.out.println("Welcome to this demonstration");
         System.out.println("__________________________________");
         System.out.println("| 1. Euclids algorithm           |");
@@ -72,14 +73,16 @@ public class Main {
         System.out.println("| 5. Passord generator           |");
         System.out.println("| 6. Fibonacci sequence          |");
         System.out.println("| 7. pascals triangle(wip)       |");
-        System.out.println("| 8. Exit                        |");
+        System.out.println("| 8. Tax calculator              |");
+        System.out.println("| 9. Exit                        |");
         System.out.println("|________________________________|");
-        int choice = getInt("Enter your choice: "); //gets the users choice
-        choice(choice); //calls the choice method
+        int choice = getInt("Enter your choice: "); // gets the users choice
+        choice(choice); // calls the choice method
 
     }
-    private static void choice(int choice){ //the choice method
-        switch (choice){
+
+    private static void choice(int choice) { // the choice method
+        switch (choice) {
             case 1:
                 Q1.main(null);
                 break;
@@ -102,12 +105,15 @@ public class Main {
                 Q7.main(null);
                 break;
             case 8:
+                Q8.main(null);
+                break;
+            case 9:
                 System.exit(0);
                 break;
             default:
                 System.out.println("Invalid choice");
                 menu();
-        } //there is almost certainly a better way to do this, but I don't know it
+        } // there is almost certainly a better way to do this, but I don't know it
     }
-    
+
 }
---
+++ src/com/company/Q8.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Q8 {
+    public static void main(String[] args) {
+        // tax calculator for the uk that takes into account the tax free allowance and the tax bands and allows the user to enter the amount of income
+
+        double income = 0; //creates a variable for the income
+        System.out.println("woul dyou like calculate your total income from your hourly wage or your annual wage?"); // asks the user if they want to calculate their income from their hourly wage or their annual wage
+        String answer = Main.getString("enter hourly or annual"); // gets the answer from the user
+        if (answer.equals("hourly")) { // if the answer is hourly
+            double hourlywage = Main.getDouble("enter your hourly wage"); // gets the hourly wage from the user
+            double hoursPerDay = Main.getDouble("enter the number of hours you work per day"); // gets the number of hours worked per day from the user
+            double daysworked = Main.getDouble("enter the number of days you worked per week"); // gets the number of hours worked from the user
+            int daysOff = Main.getInt("enter the number of days you have taken off through the year"); // gets the number of days off from the user
+            income = (hourlywage * ((daysworked*hoursPerDay)*52)) - (hourlywage*hoursPerDay)*daysOff; // calculates the income
+        } else if (answer.equals("annual")) { // if the answer is annual
+            income = Main.getDouble("enter your annual wage"); // gets the annual wage from the user
+        } else { // if the answer is not hourly or annual
+            System.out.println("invalid answer"); // prints invalid answer
+            System.out.println("try again");
+            main(args); // restarts the program
+        }
+        double tax = 0; //creates a variable for the tax
+        if (income <= 12500) { //checks if the income is less than or equal to the tax free allowance
+            tax = 0; //sets the tax to 0
+        } else if (income <= 50000) { //checks if the income is less than or equal to the first tax band
+            tax = (income - 12500) * 0.2; //calculates the tax
+        } else if (income <= 150000) { //checks if the income is less than or equal to the second tax band
+            tax = 7500 + (income - 50000) * 0.4; //calculates the tax
+        } else { //checks if the income is greater than the second tax band
+            tax = 47500 + (income - 150000) * 0.45; //calculates the tax
+        }
+        System.out.println("your total income is £" + income); //prints the income
+        System.out.println("The tax is £" + tax); //prints the tax
+
+        System.out.println("your remaining income is £" + (income - tax)); //prints the remaining income
+        Main.menu(); //calls the menu method
+    }
+
+}
